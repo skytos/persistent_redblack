7138a114f036e9dfd1e03555608d011827d5ef22
Skylar Broad <skylar.broad@gmail.com>
2021-01-25 16:16:50 -0800

    add tests

--- tree.js
+++ tree.js
@@ -1,13 +1,19 @@
-add = (t, x) =>
+add = (x, t) =>
     !t ? {val: x} :
     x === t.val ? t :
-    x < t.val ? {...t, left: add(t.left, x)} :
-    {...t, right: add(t.right, x)}
+    x < t.val ? {...t, left: add(x, t.left)} :
+    {...t, right: add(x, t.right)}
 
-find = (t, x) =>
+find = (x, t) =>
     !!t && (
         x === t.val ||
-        x < t.val && find(t.left, x) ||
-        x > t.val && find(t.right, x)
+        x < t.val && find(x, t.left) ||
+        x > t.val && find(x, t.right)
     )
 
+nums = [1,5,4,7,6]
+outnums = [2,8,3,0]
+t = nums.reduce((t, x) => add(x, t), null)
+console.log(t)
+console.log(nums.map(x => find(x, t)))
+console.log(outnums.map(x => find(x, t)))
